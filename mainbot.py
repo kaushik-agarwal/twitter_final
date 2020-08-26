e747e685c0608718ea72ad1c89e18fc419ff56ae
kaushik-agarwal <70212372+kaushik-agarwal@users.noreply.github.com>
2020-08-26 20:41:11 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -35,6 +35,3 @@ while True:
     print(f'Updating Twitter Name : Kaushik|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
-   
-                     
-          
